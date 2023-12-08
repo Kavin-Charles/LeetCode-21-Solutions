4643d470262f360712ec105fe09c86b0fabb6dba
Kavin Charles <87717754+Kavin-Charles@users.noreply.github.com>
2023-12-08 21:04:52 +0530

    Create 35-search-insert-position.cpp

---
+++ 35-search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    #define DPSolver ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int searchInsert(vector<int>& nums, int trgt) {
+        DPSolver;
+        int b = 0; int e = nums.size(); int m = (e+b) / 2; 
+        while (e > b){
+            if (nums[m] == trgt)
+                return m; 
+            else if (nums[m] > trgt)
+                e = m; 
+            else 
+                b = m + 1; 
+            m = (b+e)/ 2;
+        }
+        return m; 
+    }
+};
